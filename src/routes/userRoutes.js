1011c660b076ed07f47703881bcaff3925d0f471
Kalp-Chaniyara <kalpchaniyara.2006@gmail.com>
2024-10-30 23:50:51 +0530

    add new routes for the user controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, logInUser, logOutUser, refreshAccessToken } from "../controllers/userControllers.js";
+import { registerUser, logInUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage, userChannelProfile, getWatchHistory } from "../controllers/userControllers.js";
 import { upload } from "../middlewares/multer.middlewares.js"
 import { verifyJWTAndGetUser } from "../middlewares/auth.middleware.js"
 
@@ -25,4 +25,15 @@ router.route("/logout").post(verifyJWTAndGetUser, logOutUser)
 
 router.route("/refresh-access-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWTAndGetUser,changeCurrentPassword)
+
+router.route("/me").get(verifyJWTAndGetUser,getCurrentUser)
+
+router.route("/avatar").patch(verifyJWTAndGetUser,upload.single("avatar"),updateUserAvatar) //se patch to modied the only which we want if we use put then the whole things will be mofied
+
+router.route("/cover-image").patch(verifyJWTAndGetUser,upload.single("coverImage"),updateUserCoverImage) //se patch to modied the only which we want if we use put then the whole things will be mofied
+
+router.route("/channel/:username").get(verifyJWTAndGetUser,userChannelProfile) //here we use username by req.params means we pass it in the url so write it after :
+
+router.route("/history").get(verifyJWTAndGetUser,getWatchHistory)
 export default router 
